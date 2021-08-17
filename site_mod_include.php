a7280ec260404e6d1ae2cabdfc97d5e6a08af711
Lançamentos Online <web@lancamentosonline.com.br>
2021-08-17 15:56:12 -0400

    erros on

--- site_mod_include.php
+++ site_mod_include.php
@@ -2,4 +2,5 @@
 require_once("sistema/ferramenta/configuracoes.php");
 require_once("sistema/ferramenta/funcao_php.php");
 require_once("sistema/ferramenta/Class/pdo.class.php");
+ini_set('display_errors', 1);
 ?>
